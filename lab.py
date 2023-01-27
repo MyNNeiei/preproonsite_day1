93c8ae347e10d6401a72909dfd44bbbaf02464fd
Kullanan Sunan <90614733+MyNNeiei@users.noreply.github.com>
2023-01-27 12:31:16 +0700

    Add files via upload

---
+++ lab.py
@@ -0,0 +1,41 @@
+"""munn pai mud leaw"""
+class DataNode:
+    def __init__(self, input_name = ""):
+        self.name = input_name
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.count = 0
+        self.head = None
+    def traverse(self):
+        if self.head is None:
+            print("This is an empty list.")
+        else:
+            pos = self.head
+            while pos is not None:
+                print('->', pos.name , end=" ")
+                pos = pos.next
+            print("")
+    def insertFornt(self, data):
+        pNew = DataNode(data)
+        if self.head is None:
+            self.head = pNew
+        else:
+            pNew.next = self.head
+            self.head = pNew
+        self.count += 1
+        return
+    def insertLast(self, data):
+        pNew = DataNode(data)
+
+mylist = SinglyLinkedList()
+pNew = DataNode("John")
+mylist.head = pNew
+print(mylist.head.name)
+pNew = DataNode("Tony")
+mylist.head.next = pNew
+print(mylist.head.next.name)
+mylist.traverse()
+pNew = DataNode("Bill")
+mylist.head.next.next = pNew
